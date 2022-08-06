9d506275953bda8c19b939ecae3c1e7217afe4cd
Kim <kimlindbla@gmail.com>
2022-08-06 11:33:45 +0200

    Added macroquad, with entry point as well as main loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn window_config() -> Conf {
+	return Conf { 
+		window_title: "Invaders".to_owned(),
+		fullscreen: true, 
+		window_resizable: false,
+		..Default::default()
+	}
+}
+
+#[macroquad::main(window_config)]
+async fn main() {
+
+	while !is_key_pressed(KeyCode::Escape) {
+		clear_background(BLACK);
+
+		draw_line(0.0, 0.0, screen_width(), screen_height(), 2.0, WHITE);
+
+		next_frame().await;
+	}
 }
